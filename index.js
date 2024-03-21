b99ab578b0b9998720954e5fb25414a8a0bdcd6a
Asdzmvc <1yulchievasadbek0071@gmail.com>
2024-03-21 18:04:19 +0500

    some cahnges

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let randomNum = Math.floor(Math.random() * 100);
-console.log(randomNum);
+// let randomNum = Math.floor(Math.random() * 100);
+// console.log(randomNum);
 
 // let number1 = "40";
 // console.log(number1); 
+
+
+const myButton = documentGetElementById("myButton");
+const myLabel = documentGetElementById("myButton");
+const min = 1;
+const max = 10;
+let randomNum;
