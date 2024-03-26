d10e3f7ee44472323d18a1fd6b5380c90583717a
Asdzmvc <1yulchievasadbek0071@gmail.com>
2024-03-26 14:24:11 +0500

    some changes

--- index.js
+++ index.js
@@ -5,8 +5,21 @@
 // console.log(number1); 
 
 
-const myButton = documentGetElementById("myButton");
-const myLabel = documentGetElementById("myButton");
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
 const min = 1;
 const max = 10;
-let randomNum;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min
+    randomNum2 = Math.floor(Math.random() * max) + min
+    randomNum3 = Math.floor(Math.random() * max) + min
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
